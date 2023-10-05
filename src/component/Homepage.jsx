813ffb842abffd394b039bef470444481f7fc3e3
Muskan <muskantolani7@gmail.com>
2023-10-05 05:39:10 +0530

    basic homepage

---
+++ src/component/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Homepage.css';
+
+export const Homepage = () => {
+  return (
+    <div className='Homepage-card'>
+        <button className='explore'>Explore</button>
+    </div>
+  )
+}
